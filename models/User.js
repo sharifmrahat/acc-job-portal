24a22daaeff72ec0dc8e65bbcbc2c6d9ad5eb6b9
ShareefRahat <imr4h47@gmail.com>
2022-10-17 21:23:54 +0600

    user schema is created

---
+++ models/User.js
@@ -0,0 +1,80 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      validate: [validator.isEmail, "Please Provide a valid Email"],
+      trim: true,
+      lowercase: true,
+      unique: true,
+      required: [true, "Email address is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      validate: {
+        validator: (value) =>
+          validator.isStrongPassword(value, {
+            minLength: 6,
+            minLowercase: 3,
+            minNumbers: 1,
+            minUppercase: 1,
+            minSymbols: 1,
+          }),
+        message: "Password {VALUE} is not strong enough!",
+      },
+    },
+    confirmPassword: {
+      type: String,
+      required: [true, "Please confirm your password"],
+      validate: {
+        validator: function (value) {
+          return value === this.password;
+        },
+        message: "Passwords does not match!",
+      },
+    },
+
+    role: {
+      type: String,
+      enum: ["manager", "candidate", "admin"],
+      default: "candidate",
+    },
+
+    firstName: {
+      type: String,
+      required: [true, "Please provide your first name"],
+      trim: true,
+      minLength: [3, "Name must be at least 3 characters."],
+      maxLength: [100, "Name is too long"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Please provide your last name"],
+      trim: true,
+      minLength: [3, "Name must be at least 3 characters."],
+      maxLength: [100, "Name is too long"],
+    },
+    contactNumber: {
+      type: String,
+      validate: [validator.isMobilePhone, "Please provide a valid contact number"],
+    },
+    imageURL: {
+      type: String,
+      validate: [validator.isURL, "Please provide a valid url"],
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+      }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
