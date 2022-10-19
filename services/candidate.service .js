e0d82fdaf5f6e4162af23fc997e2bfdfda6b8285
ShareefRahat <imr4h47@gmail.com>
2022-10-19 19:30:36 +0600

    candidate service is updated

--- services/candidate.service .js
+++ services/candidate.service .js
@@ -1,13 +1,13 @@
 const Candidate = require("../models/Candidate");
 
 exports.createCandidateService = async (data) => {
-    const candidate = await Candidate.create(data);
-    return candidate;
+    const newCandidate = await Candidate.create(data);
+    return newCandidate;
   };
 
 exports.findAllCandidateService = async (query) => {
-    const candidate = await Candidate.find({...query?.data}).populate('appliedInfo');
-    return candidate;
+    const candidates = await Candidate.find({...query?.data}).populate('appliedInfo');
+    return candidates;
   };
 
   exports.findOneCandidateService = async (query) => {
